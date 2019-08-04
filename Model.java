529d40636ab7518e80e9af1d37f7046fb346e144
clintonbuehring <45835157+clintonbuehring@users.noreply.github.com>
2019-08-04 17:31:36 -0500

    Added Movie Rating & a little clean up

--- Model.java
+++ Model.java
@@ -1,10 +1,15 @@
 import javax.swing.JTextArea;
 
+//*** This Model is to just make sure the View and Controller are sending and receiving data...
+//		Similar getters would probably be needed in the actual Model, to send results to 
+//		View via the Controller.
 public class Model {
       
        private String reviewDecision = "Positive";
        private double accuracy = 0.85;
        private String polarity = "Strong";
+       private double movieRating = 4.76;
+       
 
        // dummy data to make sure it is getting sent from Model to the View, via Controller
        public String getReviewDecision() {
@@ -18,7 +23,13 @@ public class Model {
        public String getPolarity() {
               return polarity;
        }
-      
+       
+       public double getMovieRating() {
+    	   return movieRating;
+       }
+       
+       
+       //*** These functions are just to make sure the info is being passed to the Model
        // function just to make sure that the user's review is making it to the Model...
        public void printReview(String uReview) {
               System.out.println("This is your Review: " + uReview);
